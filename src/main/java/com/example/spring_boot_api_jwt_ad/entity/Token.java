cd4436a1b3a6d3debd587a06d69df5ac0d407ec0
quốc tài <qtai307@gmail.com>
2023-07-06 16:36:07 +0700

    tuan4

---
+++ src/main/java/com/example/spring_boot_api_jwt_ad/entity/Token.java
@@ -0,0 +1,37 @@
+package com.example.spring_boot_api_jwt_ad.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "t_token")
+
+public class Token extends BaseEntity {
+
+    public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public Date getTokenExpDate() {
+		return tokenExpDate;
+	}
+
+	public void setTokenExpDate(Date tokenExpDate) {
+		this.tokenExpDate = tokenExpDate;
+	}
+
+	@Column(length = 1000)
+    private String token;
+
+    private Date tokenExpDate;
+
+}
